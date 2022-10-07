63c68b79811af963f9b7308ed92c46fa5878eaae
BendeckDavid <davidgbendeck@gmail.com>
2022-10-07 00:15:35 -0500

    database init

---
+++ database.go
@@ -0,0 +1,27 @@
+package conn
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+// Start && Migrate Database
+func StartDB(path string, modules ...[]any) {
+	var err error
+
+	DB, err = gorm.Open(sqlite.Open(path), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	var migrations []any
+
+	for _, md := range modules {
+		migrations = append(migrations, md...)
+	}
+
+	// Migrate the schema
+	DB.AutoMigrate(migrations...)
+}
